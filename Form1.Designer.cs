f4462b6e887829b4b27536e6273c68fddb0e631b
rymn <Sad.121188>
2016-02-23 13:43:05 -0900

    Wrong files
    Uploading the correct files here.  This is V1.2

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace GCI_Data_Usage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_Username = new System.Windows.Forms.TextBox();
+            this.textBox_Password = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(268, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(671, 524);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Credentials from login.gci.com";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "User";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Pass";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(194, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Hours between usage checks";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "6",
+            "12",
+            "24",
+            "48"});
+            this.comboBox1.Location = new System.Drawing.Point(19, 143);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(47, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Save and Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox_Username
+            // 
+            this.textBox_Username.Location = new System.Drawing.Point(87, 36);
+            this.textBox_Username.Name = "textBox_Username";
+            this.textBox_Username.Size = new System.Drawing.Size(154, 22);
+            this.textBox_Username.TabIndex = 7;
+            // 
+            // textBox_Password
+            // 
+            this.textBox_Password.Location = new System.Drawing.Point(87, 64);
+            this.textBox_Password.Name = "textBox_Password";
+            this.textBox_Password.Size = new System.Drawing.Size(154, 22);
+            this.textBox_Password.TabIndex = 8;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(253, 10);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "white",
+            "batfull27"});
+            this.comboBox2.Location = new System.Drawing.Point(19, 205);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Theme";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 299);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox_Password);
+            this.Controls.Add(this.textBox_Username);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "GCI Data Usage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_Username;
+        private System.Windows.Forms.TextBox textBox_Password;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Management.Instrumentation;
+using System.IO;
+using System.Xml.Serialization;
+using System.Threading;
+using Microsoft.Win32;
+
+namespace GCI_Data_Usage
+{
+    public partial class Form1 : Form
+    {
+        Icon[] iconArray = new Icon[101];
+        NotifyIcon activeIcon;
+        Thread gciWorker;
+        private int loginErrorCounter = 0;
+        private string internetUsageCap;
+        private string internetUsageTotal;
+        private string wirelessUsageCap;
+        private string wirelessUsageTotal;
+        private int internetPrecentage = Int32.Parse(Settings1.Default["usage"].ToString());
+
+        public Form1()
+        {
+            #region Initialization Data (username and password)
+            InitializeComponent();
+            textBox_Username.Text = Settings1.Default["username"].ToString();
+            textBox_Password.Text = Settings1.Default["password"].ToString();
+            comboBox1.Text = Settings1.Default["frequency"].ToString();
+            comboBox2.Text = Settings1.Default["theme"].ToString();
+
+
+            //set icons objects
+            buildIconArray(Settings1.Default["theme"].ToString());
+
+            //create notify icon and assign idle icon and show it
+            activeIcon = new NotifyIcon();
+            activeIcon.Icon = iconArray[Int32.Parse(Settings1.Default["usage"].ToString())];
+            activeIcon.Visible = true;
+
+            //create menu items
+            MenuItem programNameMenuItem = new MenuItem("GCI internet usage BETA v1.1.2");
+            MenuItem programNameMenuItem2 = new MenuItem("Check for updates");
+            MenuItem quitMenuItem = new MenuItem("Quit");
+            ContextMenu contextMenu = new ContextMenu();  //create the contect menu
+            contextMenu.MenuItems.Add(programNameMenuItem);  //add items to the context menu
+            contextMenu.MenuItems.Add(programNameMenuItem2);  //add items to the context menu
+            contextMenu.MenuItems.Add(quitMenuItem);
+            activeIcon.ContextMenu = contextMenu;  //link context menu to the tray icon
+            //set initial icon hover text.
+            if(System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                activeIcon.Text = "Connected! Loading";
+            } else {
+                activeIcon.Text = "Waiting for internet";
+            }
+
+
+            //Make the first menu item unclickable
+            contextMenu.MenuItems[0].Enabled = false;
+
+            //click event for icon to open options
+            activeIcon.Click += activeIcon_Click;
+
+            //wire up quit button to close application
+            quitMenuItem.Click += QuitMenuItem_Click;
+
+            //wire up quit button to close application
+            programNameMenuItem2.Click += programNameMenuItem2_Click;
+
+            //Hide the form because we don't need it yet
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+
+            //start worker thread that polls GCI
+            gciWorker = new Thread(new ThreadStart(GCIActivityThread));
+            gciWorker.Start();
+
+            //adds an additional "on complete" task for the webbrowser
+            webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+            webBrowser1.Navigate("https://login.gci.com");
+            #endregion
+        }
+
+        public void buildIconArray(string name)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                iconArray[i] = new Icon(Application.StartupPath.ToString()+"/icons/" + name + "/" + i + ".ico");
+            }
+        }
+
+        private void activeIcon_Click(object sender, EventArgs e)
+        {
+            this.Activate();
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+
+        private void QuitMenuItem_Click(object sender, EventArgs e)
+        {
+            gciWorker.Abort();
+            activeIcon.Dispose();
+            this.Close();
+        }
+
+        private void programNameMenuItem2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.allthedurbin.com/gci-usage-app/");
+        }
+
+        public void GCIActivityThread()
+        {
+            //wbemtest command to add u/d telemetrics later
+            //main loop
+            while (true)
+            {
+                int internetError = 0;
+                while(!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+                {
+                    if(internetError == 0)
+                    {
+                        activeIcon.Text = "Waiting for internet";
+                        internetError = 1;
+                    }
+                    
+                    Thread.Sleep(60000);
+                }
+                webBrowser1.Navigate("https://apps.gci.com/um/overview");
+                Thread.Sleep(1000 * 60 * 60 * Int32.Parse(Settings1.Default["frequency"].ToString()));
+            }
+        }
+
+        void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (webBrowser1.Document.Url.Equals("https://login.gci.com"))
+            {
+                progressBar1.Value = 25;
+                if (loginErrorCounter < 2)
+                {
+                    webBrowser1.Document.GetElementById("username").SetAttribute("value", Settings1.Default["username"].ToString());
+                    webBrowser1.Document.GetElementById("password").SetAttribute("value", Settings1.Default["password"].ToString());
+                    webBrowser1.Document.GetElementById("login").InvokeMember("click");
+                    loginErrorCounter += 1;
+                }
+                else
+                {
+                    //This will display the login page if it cannot log in, no matter what.
+                    errorStop("Unable To Login, Check your username and password then click save");
+                }
+            }
+
+            if (webBrowser1.Document.Url.Equals("https://www.gci.com/my-gci"))
+            {
+                progressBar1.Value = 50;
+                webBrowser1.Navigate("https://apps.gci.com/um/overview");
+            }
+
+            if (webBrowser1.Document.Url.Equals("https://apps.gci.com/um/overview"))
+            {
+                progressBar1.Value = 75;
+                //uses javascript to get the value of the internet cap, including extra buckets.
+                internetUsageCap = webBrowser1.Document.InvokeScript("eval", new[] {
+    "(function() { var internetCap=0;$.each($('#internet .cap'), function(index, value){internetCap=internetCap + parseInt(value.innerHTML);});return String(internetCap); })()" }).ToString();
+                internetUsageTotal = webBrowser1.Document.InvokeScript("eval", new[] {
+    "(function() { var internetTotal=0;$.each($('#internet .total'), function(index, value){internetTotal=internetTotal + parseFloat(value.innerHTML);});return String(internetTotal);})()" }).ToString();
+
+                wirelessUsageCap = webBrowser1.Document.InvokeScript("eval", new[] {
+    "(function() { var wirelessCap=0;$.each($('#shared_mobile .cap'), function(index, value){wirelessCap=wirelessCap + parseFloat(value.innerHTML);return false;});return String(wirelessCap);})()" }).ToString();
+                wirelessUsageTotal = webBrowser1.Document.InvokeScript("eval", new[] {
+    "(function() { var wirelessTotal=0;$.each($('#shared_mobile .total'), function(index, value){wirelessTotal=wirelessTotal + parseFloat(value.innerHTML);return false;});return String(wirelessTotal);})()" }).ToString();
+
+                int internetPrecentage = Convert.ToInt32(Math.Round(Convert.ToDouble(internetUsageTotal))) * 100 / (Int32.Parse(internetUsageCap)) ;
+                activeIcon.Icon = iconArray[internetPrecentage];
+                Settings1.Default["usage"] = Int32.Parse(internetPrecentage.ToString());
+                Settings1.Default.Save();
+                activeIcon.Text = "Home Internet: "+internetUsageTotal.ToString()+"GB /"+ internetUsageCap.ToString()+" GB";
+                if(wirelessUsageCap.ToString() != "0")
+                {
+                    activeIcon.Text += "\n";
+                    activeIcon.Text += "Wireless: "+wirelessUsageTotal.ToString() + "GB /" + wirelessUsageCap.ToString() + " GB";
+                }
+                
+                progressBar1.Value = 100;
+                Thread.Sleep(100);
+                progressBar1.Value = 0;
+            }
+        }
+
+        private void errorStop(string message)
+        {
+            //bring window to front, unminimize, focus
+            MessageBox.Show(message);
+            progressBar1.Value = 0;
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+
+        //override the close button to ask if you want to close. most people want to minimze
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            if (e.CloseReason == CloseReason.WindowsShutDown) return;
+
+            // Confirm user wants to close
+            switch (MessageBox.Show(this, "Are you sure you want to close?", "Minimizing", MessageBoxButtons.YesNo))
+            {
+                case DialogResult.No:
+                    e.Cancel = true;
+                    break;
+                default:
+                    activeIcon.Dispose();
+                    gciWorker.Abort();
+                    break;
+            }
+        }
+
+        //override default minimze action to hide the app again
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        //save button clicked
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Settings1.Default["username"] = textBox_Username.Text.ToString();
+            Settings1.Default["password"] = textBox_Password.Text.ToString();
+            Settings1.Default.Save();
+            Application.Restart();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(activeIcon != null)
+            {
+               Settings1.Default["theme"] = comboBox2.Text.ToString();
+               buildIconArray(comboBox2.Text.ToString());
+               activeIcon.Icon = iconArray[internetPrecentage];
+
+            } 
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Settings1.Default["frequency"] = Int32.Parse(comboBox1.Text.ToString());
+        }
+    }
+}
